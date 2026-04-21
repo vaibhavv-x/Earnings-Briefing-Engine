7945988ada32553e77a7d41b119229eb6f556c9c
Vaibhav <vaibhav_ravikumar@srmap.edu.in>
2026-04-21 15:59:36 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Ensure backend matches this
+  baseURL: import.meta.env.VITE_API_URL, // Ensure backend matches this
 });
 
 export const getCompanyInfo = async (symbol) => {
